784b7e64d214f6c0f8456d945ed4cf8113313181
BolajiOlajide <bolaji.olajide@andela.com>
2019-01-22 14:16:38 +0100

    add sql insert example

--- sample.sql
+++ sample.sql
@@ -1,5 +1,15 @@
-CREATE TABLE test_table (
+-- CREATE A TABLE with the columns id, firstName and lastName
+CREATE TABLE people (
     id INTEGER,
     firstName VARCHAR(255),
     lastName VARCHAR(255)
 );
+
+-- Used to delete a table from the database
+DROP TABLE people;
+
+-- insert records into table
+INSERT INTO people VALUES (1, 'John', 'Doe');
+INSERT INTO people (id, lastName, firstName) VALUES (2, 'Travis', 'Garland');
+INSERT INTO people VALUES (3, 'Jane', 'Doe');
+INSERT INTO people (id, lastName, firstName) VALUES (4, 'Corey', 'Schafer');
